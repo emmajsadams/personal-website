6e7987b5d4109c2507b5f17efc0b970e0f48b0e5
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-14 23:50:52 -0700

    Add test emotions styles

---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import { css } from '@emotion/css'
+import tw from '@tailwindcssinjs/macro'
+import HeaderStyled from '../components/HeaderStyled'
+
+const Test = () => (
+  <div className={css(tw`grid justify-center items-center h-screen`)}>
+    <HeaderStyled>@emotion/css</HeaderStyled>
+  </div>
+)
+
+export default Test
