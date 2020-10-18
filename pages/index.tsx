1fd896e05d0ed65df7a28051ec4741e4341f646e
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-18 01:10:31 -0700

    Remove bootstrap from index in favor of tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,30 @@
 import Head from "next/head";
 import Link from "next/link";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import styles from "../styles/Home.module.css"; // TODO: page level css vs component css
 
 export default function Resume() {
   return (
-    <Container fluid="md">
+    <div className="container">
       <Head>
         <title>Emma Stebbins</title>
-        <meta
-          name="google-site-verification"
-          content="O_dhy7q1Kfwrvh4v9kyyHmHVZWJqzCegjrgNteyU65k"
-        />
-        <link href="/emma.jpg" rel="icon" type="image/jpg"></link>
       </Head>
-      <h1 className={styles.header}>Emma Stebbins</h1>
+      <h1>Emma Stebbins</h1>
       <p>
         Full-stack software engineer experienced with C#, Python, JavaScript.
         Seattle, WA or Remote.
       </p>
-      <Row>
-        <Col md={3}>
-          <h3 className={styles.header}>Education</h3>
+      <div className="md:flex">
+        <div className="md:w-1/3">
+          <h2>Education</h2>
           <p>University of Washington — B.S, Informatics, 2014</p>
           <p>DigiPen Institute of Technology — Computer Engineering, 2011</p>
-          <h3 className={styles.header}>Languages</h3>
+          <h2>Languages</h2>
           <p>JavaScript, CSS, HTML, C#, Python, SQL</p>
-          <h3 className={styles.header}>Architecture</h3>
+          <h2>Architecture</h2>
           <p>
             Object oriented design, functional programming, REST API design,
             Unit & integration Testing, MVC & MVVM
           </p>
-          <h3 className={styles.header}>Open Source Portfolio</h3>
+          <h2>Open Source Portfolio</h2>
           <p>
             <a href="https://github.com/emmastebbins/blocktube">sharetube</a> -
             Sharing a redundantly uploaded video using TypeScript, React, Redux,
@@ -47,7 +38,7 @@ export default function Resume() {
             <a href="https://github.com/emmastebbins/CheckOut">CheckOut</a> -
             Store inventory manager using C# and WebAPI
           </p>
-          <h3 className={styles.header}>Links</h3>
+          <h2>Links</h2>
           <ul>
             <li>
               <a href="/Emma_Stebbins_Resume.pdf">PDF Resume</a>
@@ -68,24 +59,23 @@ export default function Resume() {
               <a href="https://twitter.com/EmmaJStebbins">Twitter</a>
             </li>
           </ul>
-        </Col>
-        <Col md={9}>
-          <h3 className={styles.header}>Experience</h3>
-
-          <h4>
+        </div>
+        <div className="md:w-2/3">
+          <h2>Experience</h2>
+          <h3>
             Freelance Software Engineer, Remote &mdash; June 2020 - Present
-          </h4>
+          </h3>
           <ul>
             <li>
               A variety of contract positions focused on full stack web
               development
             </li>
           </ul>
-          <h4>
+          <h3>
             Lead Senior Software Engineer II,&nbsp;
             <a href="https://www.humblebundle.com">Humble Bundle</a> &mdash;
             Remote &mdash; March 2019 - June 2020
-          </h4>
+          </h3>
           <ul>
             <li>
               Promoted from Senior Software Engineer to Lead Senior Software
@@ -114,11 +104,11 @@ export default function Resume() {
             </li>
           </ul>
 
-          <h4>
+          <h3>
             Lead Full Stack Software Engineer,&nbsp;
             <a href="https://www.cfchildren.org">Committee for Children</a>
             &mdash; Seattle &mdash; Jan 2018 - June 2018
-          </h4>
+          </h3>
           <ul>
             <li>
               Lead replacement of a legacy monolithic DNN platform over to using
@@ -134,11 +124,11 @@ export default function Resume() {
             </li>
           </ul>
 
-          <h4>
+          <h3>
             Full Stack Software Engineer,&nbsp;
             <a href="https://www.trov.com/">Trov</a> &mdash; Remote &mdash; July
             2013 – Sept 2017
-          </h4>
+          </h3>
           <ul>
             <li>
               Remote software development team where developers write unit and
@@ -158,11 +148,11 @@ export default function Resume() {
             </li>
           </ul>
 
-          <h4>
+          <h3>
             Assistant Researcher,&nbsp;
             <a href="https://ischool.uw.edu">UW Information School</a> &mdash;
             Jan 2013 – Oct 2013
-          </h4>
+          </h3>
           <ul>
             <li>
               Lead developer for a JavaScript library which abstracted cloud
@@ -174,20 +164,20 @@ export default function Resume() {
             </li>
           </ul>
 
-          <h4>
+          <h3>
             Software Engineer Intern,&nbsp;
             <a href="//www.optionshouse.com/">PEAK6 Investments LP</a> &mdash;
             Sept 2012 – May 2013
-          </h4>
+          </h3>
           <ul>
             <li>Maintained legacy Java and Ruby on Rails stock trading apps</li>
           </ul>
 
-          <h4>
+          <h3>
             Web Instructor and Engineer,&nbsp;
             <a href="//www.washington.edu/lst/workshops/">UW IT</a> &mdash; Feb
             2012 – Mar 2013
-          </h4>
+          </h3>
           <ul>
             <li>
               Taught modern web development standards to UW staff and students
@@ -198,29 +188,29 @@ export default function Resume() {
             </li>
           </ul>
 
-          <h4>
+          <h3>
             Software Engineer Intern,&nbsp;
             <a href="//www.popshops.com/">PopShops.com</a> &mdash; June 2012 –
             Aug 2012
-          </h4>
+          </h3>
           <ul>
             <li>
               Scraped and normalized product information using Ruby and Java
             </li>
           </ul>
 
-          <h4>
+          <h3>
             Software Engineer Intern,&nbsp;
             <a href="//www.smiteworks.com/">SmiteWorks</a> &mdash; June 2011 –
             Aug 2011
-          </h4>
+          </h3>
           <ul>
             <li>
               Built a C# app to import character data from competitors apps
             </li>
           </ul>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 }
