b74116262780bd02fa71bfb4df73a2ef628958b1
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-18 02:13:09 -0700

    add todo for fix typing

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
+// TODO: move this to separate file and fix typing
 // types/mdx.d.ts
 declare module '*.mdx' {
   let MDXComponent: (props: any) => JSX.Element
