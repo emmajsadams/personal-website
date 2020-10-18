7b40026c67cb16ce4ce022d9ba7146afcf0203fb
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-18 00:47:18 -0700

    Add mdx support to project for blog posts

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// next.config.js
+const withMdxEnhanced = require('next-mdx-enhanced')
+
+
+module.exports = withMdxEnhanced({
+  layoutPath: 'layouts',
+  defaultLayout: true,
+  fileExtensions: ['mdx'],
+  remarkPlugins: [],
+  rehypePlugins: [],
+  usesSrc: false,
+  extendFrontMatter: {
+    process: (mdxContent, frontMatter) => {},
+    phase: 'prebuild|loader|both',
+  },
+  reExportDataFetching: false,
+})(/* your normal nextjs config */)
