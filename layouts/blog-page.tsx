095a4c2284729e125304b55755d8ef9e93d0ce3b
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-18 01:10:10 -0700

    add mdx blog layout

---
+++ layouts/blog-page.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
+import { MDXProvider } from "@mdx-js/react";
+import CodeBlock from "../components/CodeBlock";
+
+const components = {
+  code: CodeBlock,
+};
+
+export default function BlogPage({ children, frontMatter }) {
+  const router = useRouter();
+
+  // TODO: remove this once fallback is turned off
+  if (router.isFallback) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <MDXProvider components={components}>
+      <Head>
+        <title>{frontMatter.title}</title>
+      </Head>
+      <div className="container">
+        <h1>{frontMatter.title}</h1>
+        <h2>{frontMatter.date}</h2>
+        <hr />
+        {children}
+        <hr />
+        <Link href="/blog">Back to Blog Index</Link>
+        <br />
+        <Link href="/">Back to Resume</Link>
+      </div>
+    </MDXProvider>
+  );
+}
