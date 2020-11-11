9f499c1a78ce2197ce4ba396744586e6a114f965
EmmaJCline <emma@cline.engineer>
2020-11-11 19:24:38 +0000

    add gtag utils for google analytics tracking

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,11 +4,17 @@
 // TODO: move this to separate file and fix typing
 // types/mdx.d.ts
 declare module '*.mdx' {
-  let MDXComponent: (props: any) => JSX.Element
-  export default MDXComponent
-  export const frontMatter = {
-    title: string,
-    date: string,
-    __resourcePath: string,
-  }
+	let MDXComponent: (props: any) => JSX.Element
+	export default MDXComponent
+	export const frontMatter = {
+		title: string,
+		date: string,
+		__resourcePath: string,
+	}
+}
+
+declare global {
+	interface Window {
+		gtag: any // TODO better type here
+	}
 }
