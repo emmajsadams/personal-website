fcded8c44153691781b94aa9c564eda54bd6a18e
EmmaJCline <emma@cline.engineer>
2020-11-11 19:26:37 +0000

    Add eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended',
+		'plugin:import/errors',
+		'plugin:react/recommended',
+	],
+	rules: {
+		'import/no-unresolved': 'off',
+	},
+}
