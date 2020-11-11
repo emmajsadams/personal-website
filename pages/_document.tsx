3d8d49c303ef39bcab572e541adadd33691cf23d
EmmaJCline <emma@cline.engineer>
2020-11-11 19:33:58 +0000

    add document for google analytics setup

---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import React, { ReactElement } from 'react'
+
+export default class MyDocument extends Document {
+	render(): ReactElement {
+		return (
+			<Html>
+				<Head>
+					<meta
+						name="google-site-verification"
+						content="O_dhy7q1Kfwrvh4v9kyyHmHVZWJqzCegjrgNteyU65k"
+					/>
+					<link href="/emma.jpg" rel="icon" type="image/jpg"></link>
+					{/* Global Site Tag (gtag.js) - Google Analytics */}
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`}
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+						}}
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
+}
