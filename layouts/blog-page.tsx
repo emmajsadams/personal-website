93417b0b188b4c90490529a1917596d8cd8cd6e2
EmmaJCline <emma@cline.engineer>
2020-11-19 05:47:57 +0000

    Switch to content and type blog page

--- layouts/blog-page.tsx
+++ layouts/blog-page.tsx
@@ -1,36 +1,50 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import Head from 'next/head'
 import { MDXProvider } from '@mdx-js/react'
+import Head from 'next/head'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React, { ReactElement } from 'react'
 import CodeBlock from '../components/CodeBlock'
 
 const components = {
-  code: CodeBlock,
+	code: CodeBlock,
 }
 
-export default function BlogPage({ children, frontMatter }) {
-  const router = useRouter()
+interface FrontMatter {
+	title: string
+	date: string
+}
 
-  // TODO: remove this once fallback is turned off
-  if (router.isFallback) {
-    return <p>Loading...</p>
-  }
+interface BlogPageProps {
+	children: ReactElement
+	frontMatter: FrontMatter
+}
 
-  return (
-    <MDXProvider components={components}>
-      <Head>
-        <title>Emma Cline - {frontMatter.title}</title>
-      </Head>
-      <div className="container">
-        <h1>{frontMatter.title}</h1>
-        <h2>{frontMatter.date}</h2>
-        <hr />
-        {children}
-        <hr />
-        <Link href="/blog">Back to Blog Index</Link>
-        <br />
-        <Link href="/">Back to Resume</Link>
-      </div>
-    </MDXProvider>
-  )
+export default function BlogPage({
+	children,
+	frontMatter,
+}: BlogPageProps): ReactElement {
+	const router = useRouter()
+
+	// TODO: remove this once fallback is turned off
+	if (router.isFallback) {
+		return <p>Loading...</p>
+	}
+
+	return (
+		<MDXProvider components={components}>
+			<Head>
+				<title>Emma Cline - {frontMatter.title}</title>
+			</Head>
+			<div className="content">
+				<h1>{frontMatter.title}</h1>
+				<h2>{frontMatter.date}</h2>
+				<hr />
+				{children}
+				<hr />
+				<Link href="/blog">Back to Blog Index</Link>
+				<br />
+				<Link href="/">Back to Resume</Link>
+			</div>
+		</MDXProvider>
+	)
 }
