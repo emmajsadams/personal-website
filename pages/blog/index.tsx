bdba54b8aa732e1908c8a28a72bc2d8c857eca5a
EmmaJCline <emma@cline.engineer>
2020-11-19 05:48:08 +0000

    Type and switch to content for blog index

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,42 +1,43 @@
-import Link from 'next/link'
 import Head from 'next/head'
-
+import Link from 'next/link'
+import React, { ReactElement } from 'react'
 // TODO: import glob these
 import { frontMatter as astarData } from './astar-csharp.mdx'
 import { frontMatter as dstructErrorData } from './dstruct-error-list-map-tree.mdx'
 import { frontMatter as dstructPosterData } from './dstruct-poster.mdx'
 import { frontMatter as dstructSetData } from './dstruct-set-multiset-stack.mdx'
 import { frontMatter as dstructTablesData } from './dstruct-tables-queues.mdx'
+
 const BLOG_POSTS = [
-  astarData,
-  dstructErrorData,
-  dstructPosterData,
-  dstructSetData,
-  dstructTablesData,
+	astarData,
+	dstructErrorData,
+	dstructPosterData,
+	dstructSetData,
+	dstructTablesData,
 ]
 
 // TODO: read mdx files in the blog folder rather than manually creating links
-export default function Blogs() {
-  return (
-    <>
-      <Head>
-        <title>Emma Cline's Blog</title>
-      </Head>
-      <div className="container">
-        <h1>Blog Posts</h1>
-        <hr />
-        <ul>
-          {BLOG_POSTS.map((post) => (
-            <li key={post.title}>
-              <Link href={post.__resourcePath.replace('.mdx', '')}>
-                {post.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <hr />
-        <Link href="/">Back to Résumé</Link>
-      </div>
-    </>
-  )
+export default function Blogs(): ReactElement {
+	return (
+		<>
+			<Head>
+				<title>Emma Cline's Blog</title>
+			</Head>
+			<div className="content">
+				<h1>Blog Posts</h1>
+				<hr />
+				<ul>
+					{BLOG_POSTS.map((post) => (
+						<li key={post.title}>
+							<Link href={post.__resourcePath.replace('.mdx', '')}>
+								{post.title}
+							</Link>
+						</li>
+					))}
+				</ul>
+				<hr />
+				<Link href="/">Back to Résumé</Link>
+			</div>
+		</>
+	)
 }
