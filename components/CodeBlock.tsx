a52cf4c965159a4fdaa953e9cfd16853d456f6a4
EmmaJCline <emma@cline.engineer>
2020-11-19 18:30:08 +0000

    Fix csharp code block rendering

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import Highlight, { defaultProps } from 'prism-react-renderer'
+import Highlight, { defaultProps, Language } from 'prism-react-renderer'
+import React, { ReactElement } from 'react'
+import '../utils/setupPrism'
 
-export default function CodeBlock({ children, className }) {
-  const language = className.replace(/language-/, '')
+interface CodeBlockProps {
+	children: string
+	className: string
+}
 
-  return (
-    <Highlight {...defaultProps} code={children.trim()} language={language}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
-  )
+export default function CodeBlock({
+	children,
+	className,
+}: CodeBlockProps): ReactElement {
+	const language = className.replace(/language-/, '') as Language
+
+	return (
+		<Highlight {...defaultProps} code={children.trim()} language={language}>
+			{({ className, style, tokens, getLineProps, getTokenProps }) => (
+				<pre className={className} style={{ ...style, padding: '20px' }}>
+					{tokens.map((line, i) => (
+						<div key={i} {...getLineProps({ line, key: i })}>
+							{line.map((token, key) => (
+								<span key={key} {...getTokenProps({ token, key })} />
+							))}
+						</div>
+					))}
+				</pre>
+			)}
+		</Highlight>
+	)
 }
