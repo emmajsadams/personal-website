5efb668c2993c414cd5f817646978461e8ef558b
EmmaJCline <emma@cline.engineer>
2020-11-20 17:24:17 +0000

    Add personal details config

---
+++ config/personalDetails.ts
@@ -0,0 +1,7 @@
+// TODO: Use this in the latex and html resume
+export const NAME = 'Emma Cline'
+export const PHONE_NUMBER = '+1-669-666-4508'
+export const EMAIL = 'emma@cline.engineer'
+export const PERSONAL_WEBSITE = 'emma.cline.engineer'
+export const LINKEDIN = 'linkedin.com/in/EmmaJCline'
+export const GITLAB = 'gitlab.com/EmmaJCline'
