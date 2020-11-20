fe65e6069479bca66c26570ee77ac044eca91205
EmmaJCline <emma@cline.engineer>
2020-11-20 22:19:35 +0000

    Fix latex build process to go into files directory and clean up fully

---
+++ scripts/latex.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -f public/files/EmmaJCline-resume.tex
+rm -f public/files/EmmaJCline-resume.pdf
+yarn run generate-latex
+xelatex ./public/files/EmmaJCline-resume.tex
+mv EmmaJCline-resume.pdf ./public/files/EmmaJCline-resume.pdf
+rm -f EmmaJCline-resume.log 
+rm -f texput.log
+rm -f EmmaJCline-resume.out
+rm -f EmmaJCline-resume.aux
+rm -f public/files/EmmaJCline-resume.aux
+rm -f public/files/EmmaJCline-resume.fdb_latexmk
+rm -f public/files/EmmaJCline-resume.fls
+rm -f public/files/EmmaJCline-resume.log
+rm -f public/files/EmmaJCline-resume.out
+rm -f public/files/EmmaJCline-resume.synctex.gz
