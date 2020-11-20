f1df2c9938623a67b24266752e3c95623b4ea5fa
EmmaJCline <emma@cline.engineer>
2020-11-20 03:59:19 +0000

    Move blog posts to config module

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,22 +2,8 @@ import Head from 'next/head'
 import Link from 'next/link'
 import React, { ReactElement } from 'react'
 import Header from '../../components/Header'
-// TODO: import glob these
-import { frontMatter as astarData } from './astar-csharp.mdx'
-import { frontMatter as dstructErrorData } from './dstruct-error-list-map-tree.mdx'
-import { frontMatter as dstructPosterData } from './dstruct-poster.mdx'
-import { frontMatter as dstructSetData } from './dstruct-set-multiset-stack.mdx'
-import { frontMatter as dstructTablesData } from './dstruct-tables-queues.mdx'
+import BLOG_POSTS from '../../config/blogs'
 
-const BLOG_POSTS = [
-	astarData,
-	dstructErrorData,
-	dstructPosterData,
-	dstructSetData,
-	dstructTablesData,
-]
-
-// TODO: read mdx files in the blog folder rather than manually creating links
 export default function Blogs(): ReactElement {
 	return (
 		<>
