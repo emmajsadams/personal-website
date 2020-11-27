b2a577490196ab3a0cf2952d4b8ebc4fd28a9d20
EmmaJCline <emma@cline.engineer>
2020-11-27 22:39:30 +0000

    Add git push script to regen latex and prevent push until commited

--- husky.config.js
+++ husky.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	hooks: {
-		'pre-push': 'yarn run lint && yarn run latex',
+		'pre-push': 'bash ./scripts/git-push.sh',
 	},
 }
---
+++ scripts/git-push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yarn run lint
+yarn run latex
+
+if [ -f ./public/files/EmmaJCline-resume.tex ]; then
+	# If the LaTeX file was regenerated then prevent a push so the new pdf can be generated
+	exit 2
+else
+	# If the LaTeX file was not regenerated from the yarn run latex then remove the unchanged PDF
+	rm ./public/files/EmmaJCline-resume.pdf
+fi
