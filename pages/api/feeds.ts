583ee03b2b540657ac1fd1217ee8c21465d44d6b
Emma Cline <emma@cline.engineer>
2020-11-29 21:33:56 -0800

    Fix spelling error in messsage

--- pages/api/feeds.ts
+++ pages/api/feeds.ts
@@ -4,11 +4,11 @@ export default async function handler(req, res): Promise<void> {
 	if (process.env.NODE_ENV === 'production') {
 		res
 			.status(403)
-			.json({ messsage: 'Feed generation not available in production' })
+			.json({ message: 'Feed generation not available in production' })
 		return
 	}
 
 	await generateFeeds()
-	res.status(200).json({ messsage: 'Feeds generated' })
+	res.status(200).json({ message: 'Feeds generated' })
 	return
 }
