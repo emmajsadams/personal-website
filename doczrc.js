c36aeb35ae951a59abb6dbc18856eb4ddd4e1d35
Emma Cline <emma@cline.engineer>
2020-11-30 20:08:16 -0800

    Add docz config

---
+++ doczrc.js
@@ -0,0 +1,10 @@
+export default {
+	typescript: true,
+	ignore: [
+		'astar-csharp.mdx',
+		'dstruct-error-list-map-tree.mdx',
+		'dstruct-poster.mdx',
+		'dstruct-set-multiset-stack.mdx',
+		'dstruct-tables-queues.mdx',
+	],
+}
