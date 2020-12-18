884f647417f94d1185701879bf270c9772b0ef16
Emma Cline <emma@cline.engineer>
2020-12-18 02:35:46 -0700

    change classname to sun

--- components/DarkMode.tsx
+++ components/DarkMode.tsx
@@ -29,10 +29,7 @@ export default function DarkMode(props: DarkModeProps): ReactElement {
 
 	return (
 		<>
-			<span
-				className={styles['hex-icon-sun']}
-				onClick={() => setDarkMode(!darkMode)}
-			>
+			<span className={styles.sun} onClick={() => setDarkMode(!darkMode)}>
 				<svg>
 					<path d="M19,1 Q21,0,23,1 L39,10 Q41.5,11,42,14 L42,36 Q41.5,39,39,40 L23,49 Q21,50,19,49 L3,40 Q0.5,39,0,36 L0,14 Q0.5,11,3,10 L19,1"></path>
 					<circle cx="21" cy="25" r="8"></circle>
