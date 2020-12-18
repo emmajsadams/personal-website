a3b061d411ce158c3090a797d98cfc70ac95ba54
Emma Cline <emma@cline.engineer>
2020-12-18 11:52:55 -0700

    Change darkmode to respect prefers settings

--- components/DarkMode.tsx
+++ components/DarkMode.tsx
@@ -1,35 +1,52 @@
 import React, { ReactElement, useState } from 'react'
 import styles from '../styles/DarkMode.module.css'
+import UserDarkMode, {
+	DARK_MODES,
+	DARK_THEME,
+	LIGHT_THEME,
+	THEME_ATTRIBUTE,
+	THEME_KEY,
+} from '../types/UserDarkMode'
+import getUserDarkMode from '../utils/getUserDarkMode'
 
-interface DarkModeProps {
-	darkMode: boolean
+function setTheme(selectedUserDarkMode: UserDarkMode) {
+	if (selectedUserDarkMode === UserDarkMode.selectedDarkTheme) {
+		localStorage.setItem(THEME_KEY, DARK_THEME)
+	}
+
+	if (selectedUserDarkMode === UserDarkMode.selectedLightTheme) {
+		localStorage.setItem(THEME_KEY, LIGHT_THEME)
+	}
 }
 
-function changeToLightMode(): void {
-	localStorage.setItem('theme', 'light')
-	document.body.setAttribute('data-theme', 'light')
-}
-
-function changeToDarkMode(): void {
-	localStorage.setItem('theme', 'dark')
-	document.body.setAttribute('data-theme', 'dark')
-}
-
-// TODO: Refactor this to have properties which define how to change between dark and light mode
-export default function DarkMode(props: DarkModeProps): ReactElement {
-	const [darkMode, setDarkMode] = useState(props.darkMode)
+export default function DarkMode(): ReactElement {
+	const [userDarkMode, setDarkMode]: [
+		UserDarkMode,
+		(userDarkMode: UserDarkMode) => void,
+	] = useState(getUserDarkMode())
 
 	if (process.browser) {
-		if (darkMode) {
-			changeToDarkMode()
+		if (DARK_MODES.includes(userDarkMode)) {
+			document.body.setAttribute(THEME_ATTRIBUTE, DARK_THEME)
 		} else {
-			changeToLightMode()
+			document.body.setAttribute(THEME_ATTRIBUTE, LIGHT_THEME)
 		}
+	}
+
+	let newlySelectedDarkMode = UserDarkMode.selectedDarkTheme
+	if (userDarkMode === UserDarkMode.selectedDarkTheme) {
+		newlySelectedDarkMode = UserDarkMode.selectedLightTheme
 	}
 
 	return (
 		<>
-			<span className={styles.sun} onClick={() => setDarkMode(!darkMode)}>
+			<span
+				className={styles.sun}
+				onClick={() => {
+					setTheme(newlySelectedDarkMode)
+					setDarkMode(newlySelectedDarkMode)
+				}}
+			>
 				<svg>
 					<path d="M19,1 Q21,0,23,1 L39,10 Q41.5,11,42,14 L42,36 Q41.5,39,39,40 L23,49 Q21,50,19,49 L3,40 Q0.5,39,0,36 L0,14 Q0.5,11,3,10 L19,1"></path>
 					<circle cx="21" cy="25" r="8"></circle>
