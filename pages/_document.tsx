1e0c55aad9417e042408d0a791239ca460f3ef59
Emma Cline <emma@cline.engineer>
2020-12-21 18:54:45 -0700

    Chanage analytics to comply with GDPR

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,27 +10,47 @@ export default class MyDocument extends Document {
 						name="google-site-verification"
 						content="O_dhy7q1Kfwrvh4v9kyyHmHVZWJqzCegjrgNteyU65k"
 					/>
-					{/* Global Site Tag (gtag.js) - Google Analytics */}
-					<script
-						async
-						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`}
-					/>
+					{/* Check for GDPR cookie consent before loading analytics */}
 					<script
 						dangerouslySetInnerHTML={{
 							__html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
+							function getCookie(cname) {
+								var name = cname + "=";
+								var decodedCookie = decodeURIComponent(document.cookie);
+								var ca = decodedCookie.split(';');
+								for(var i = 0; i <ca.length; i++) {
+									var c = ca[i];
+									while (c.charAt(0) == ' ') {
+										c = c.substring(1);
+									}
+									if (c.indexOf(name) == 0) {
+										return c.substring(name.length, c.length);
+									}
+								}
+								return "";
+							}
+
+							if (getCookie('CookieConsent') || getCookie('CookieConsent-legacy')) {
+								var googleTagManager = document.createElement( 'script' );
+								googleTagManager.src = 'https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}';
+								googleTagManager.async = true;
+								document.head.appendChild(googleTagManager);
+
+								var cloudflareAnalytics = document.createElement( 'script' );
+								cloudflareAnalytics.src = 'https://static.cloudflareinsights.com/beacon.min.js';
+								cloudflareAnalytics.defer = true;
+								cloudflareAnalytics.setAttribute('data-cf-beacon', '{"token": "5bea707fee3a4c3bad09c585c8f00fe8"}')
+								document.head.appendChild(cloudflareAnalytics);
+
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', {
+									page_path: window.location.pathname,
+								});
+							}
           `,
 						}}
-					/>
-					<script
-						defer
-						src="https://static.cloudflareinsights.com/beacon.min.js"
-						data-cf-beacon='{"token": "5bea707fee3a4c3bad09c585c8f00fe8"}'
 					/>
 				</Head>
 				<body data-theme="dark">
