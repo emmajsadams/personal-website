af0f3c48fc8ffee72b69b59b6f24ee0ba43e9286
Emma Cline <emma@cline.engineer>
2020-12-21 22:58:31 -0700

    Add globals.css to docz

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,4 +1,4 @@
 import React from 'react'
-import '../styles/globals.css'
+import '../../../styles/globals.css'
 
-export default ({ children }) => ({ children })
+export default ({ children }) => <>{children}</>
