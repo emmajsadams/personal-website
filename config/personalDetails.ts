693ea977f7589ab3db5b2d3a53c4da5f25fbc0d7
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2021-03-06 21:42:09 +0000

    Update email

--- config/personalDetails.ts
+++ config/personalDetails.ts
@@ -1,6 +1,6 @@
 export const NAME = 'Emma Cline'
 export const PHONE_NUMBER = '+1-669-666-4508'
-export const EMAIL = 'emma@cline.engineer'
+export const EMAIL = 'emma@adams.engineer'
 export const PERSONAL_WEBSITE = 'emma.cline.engineer'
 export const LINKEDIN = 'linkedin.com/in/EmmaJaneAdams'
 export const GITLAB = 'gitlab.com/EmmaJCline'
