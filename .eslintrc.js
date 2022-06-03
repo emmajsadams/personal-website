cdc3f9201b9b7341a1bc101656c916d9b73a96ff
Emma Adams <emma@adams.engineer>
2022-06-02 22:45:09 -0700

    update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,6 @@ module.exports = {
 		sourceType: 'module',
 	},
 	extends: [
-		'plugin:@typescript-eslint/recommended',
-		'prettier/@typescript-eslint',
 		'plugin:prettier/recommended',
 		'plugin:import/errors',
 		'plugin:react/recommended',
